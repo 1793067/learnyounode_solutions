20fec0fd7efadd9ec6c5f3436def8853e0fe90c6
1793067 <1793067@mail.ru>
2021-02-26 15:42:35 +0300

    testing receiving to github

--- chapter_08.js
+++ chapter_08.js
@@ -1,7 +1,7 @@
 const [, , url] = process.argv;
 const http = require('http');
 
-http.get(url, (response) => {
-  response.setEncoding('utf8');
-  response.on('end', data => console.log(data));
-});
+// http.get(url, (response) => {
+//   response.setEncoding('utf8');
+//   response.on('end', data => console.log(data));
+// });
